61fa9ebc2c6288cf4bb16f9b50c60093f8f9ab9f
luquibarba <vbarrio@alumno.huergo.edu.ar>
2025-09-01 22:13:49 -0300

    feat: plataforma an precioHistorico entity

---
+++ rata-gamer/src/main/java/ar/edu/huergo/rata_gamer/entity/Plataforma.java
@@ -0,0 +1,24 @@
+package ar.edu.huergo.rata_gamer.entity;
+
+import jakarta.persistence.Entity;
+//falta añadir importaciones
+
+@Entity
+@Table(name = "plataformas")
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Plataforma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nombre; 
+
+    @Column(length = 150)
+    private String url;
+
+}
+
---
+++ rata-gamer/src/main/java/ar/edu/huergo/rata_gamer/entity/PrecioHistorico.java
@@ -0,0 +1,38 @@
+package ar.edu.huergo.rata_gamer.entity;
+
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import jakarta.validation.constraints.NotNull; 
+
+
+@Entity
+@Table(name = "precios_historicos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class PrecioHistorico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull
+    private LocalDate fechaInicio;
+    
+
+    private LocalDate fechaFin;
+
+    @NotNull
+    private Double precio;
+
+}
