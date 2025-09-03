c9ff42a77385fb84690ca7dedca72897fc360ec4
Matias Budani <mbudani@alumno.huergo.edu.ar>
2025-09-02 21:36:12 -0300

    feat(Juego.java); add @EqualsAndHashCode

--- rata-gamer/src/main/java/ar/edu/huergo/rata_gamer/entity/Juego.java
+++ rata-gamer/src/main/java/ar/edu/huergo/rata_gamer/entity/Juego.java
@@ -1,5 +1,7 @@
 package ar.edu.huergo.rata_gamer.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,6 +11,7 @@ import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -16,13 +19,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@EqualsAndHashCode
 public class Juego {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "El nombre es obligatorio")
-    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+    @Size(max = 500, message = "El nombre debe tener entre 2 y 100 caracteres")
     private String nombre;
+
+    // @Size(max = 100, message = "la pla no puede exceder los 500 caracteres")
+    // private List<Plataforma> plataformas;
 }
