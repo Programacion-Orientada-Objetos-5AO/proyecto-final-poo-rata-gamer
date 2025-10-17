9951655e4bf1f5d35b721fd0048a3dde4296682f
Matias Budani <mbudani@alumno.huergo.edu.ar>
2025-10-16 21:23:37 -0300

    feat(Publicacion.java): add mapping annotations

--- rata-gamer/src/main/java/ar/edu/huergo/rata_gamer/entity/Publicaciones/Publicacion.java
+++ rata-gamer/src/main/java/ar/edu/huergo/rata_gamer/entity/Publicaciones/Publicacion.java
@@ -2,6 +2,7 @@ package ar.edu.huergo.rata_gamer.entity.publicaciones;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -10,6 +11,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
@@ -18,8 +21,6 @@ import jakarta.persistence.GenerationType;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
-
 public class Publicacion {
 
     @Id
@@ -28,12 +29,15 @@ public class Publicacion {
     private Long id;
 
     @NotNull
+    @ManyToOne  // <-- Asumiendo que muchos Publicacion pueden tener un mismo Juego
     private Juego juego;
 
     @NotNull
+    @ManyToOne  // <-- Similar para Plataforma
     private Plataforma plataforma;  
 
     @NotNull
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "publicacion") // Otras opciones si es bidireccional
     private List<PrecioHistorico> preciosHistoricos;
 
 }
