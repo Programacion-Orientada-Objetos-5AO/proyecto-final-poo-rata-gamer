d5c1501e8da9c2b1d5aa6ea6bfc3ddb5493c69e3
barrio674 <vbarrio@alumno.huergo.edu.ar>
2025-11-08 03:58:36 -0300

    feat: implement Giveaway feature with controller, service, DTOs, and updated index template

--- rata-gamer/src/main/java/ar/edu/huergo/rata_gamer/controller/web/GiveawayWebController.java
+++ rata-gamer/src/main/java/ar/edu/huergo/rata_gamer/controller/web/GiveawayWebController.java
@@ -1,14 +1,15 @@
 package ar.edu.huergo.rata_gamer.controller.web;
 
-import ar.edu.huergo.rata_gamer.dto.publicaciones.GiveawayDTO;
-import ar.edu.huergo.rata_gamer.service.publicaciones.GiveawayService;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import ar.edu.huergo.rata_gamer.dto.publicaciones.GiveawayDTO;
+import ar.edu.huergo.rata_gamer.service.publicaciones.GiveawayService;
 
 @Controller
 @RequestMapping("/web")
